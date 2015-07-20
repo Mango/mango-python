b11df66c585de36e7375ab19bddfa18f6df698f5
Juan Rossi <juan@getmango.com>
2015-07-20 12:01:52 -0300

    Updated version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="pymango",
-    version="0.1.0",
+    version="0.1.1",
     author="Mango Development Team",
     author_email="support@getmango.com",
     packages=["pymango"],
